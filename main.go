8ad1e084649ab5cd53d65e02f9df280d898ff9ce
flanker-d <flanker-d@pm.me>
2023-02-08 19:36:00 +0000

    обработка url-запроса

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Обработчик главной странице.
@@ -21,7 +23,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Обработчик для отображения содержимого заметки.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Отображение заметки..."))
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+	// и записываем его в http.ResponseWriter.
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 }
 
 // Обработчик для создания новой заметки.
